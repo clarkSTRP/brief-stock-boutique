7cc9d3b4b60bb47e5ebf880b0914cb7759a9daab
MehdiYsl <mehdi.yansli@gmail.com>
2023-02-07 12:07:36 +0100

    idk

--- index.php
+++ index.php
@@ -12,9 +12,15 @@
 </html>
 
 <?php 
+try{
 $mysqlConnection = new PDO(
     'mysql:host=localhost;dbname=VapFactory;charset=utf8',
     'admin',
     'adminpwd'
 );
+}
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+}
 ?>
