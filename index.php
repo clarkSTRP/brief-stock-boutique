9a16289aed359cd77d14ddb0b3024814b49dc9f8
MehdiYsl <mehdi.yansli@gmail.com>
2023-02-07 16:12:25 +0100

    Changement du titre et enlèvelement de la phrase

--- index.php
+++ index.php
@@ -23,11 +23,10 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>Vap Factory</title>
     </head>
     <body>
 
-        bonjour je suis content
         <table border="1" width="90%;" >
             <tbody>
                 <?php 
