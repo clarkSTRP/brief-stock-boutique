a8b5ab9f5428c56b1c1f3689655a33a2c6d657bf
MehdiYsl <mehdi.yansli@gmail.com>
2023-02-07 16:09:15 +0100

    Récupération des données de la base de donnés et création de boucles pour ajouter les valeurs dans un tableau sur la page web

--- index.php
+++ index.php
@@ -1,26 +1,46 @@
+<?php 
+    try{
+        $mysqlConnection = new PDO(
+            'mysql:host=localhost;dbname=VapFactory;charset=utf8',
+            'admin',
+            'adminpwd'
+            );
+            }
+            catch (Exception $e)
+            {
+            die('Erreur : ' . $e->getMessage());
+            }
+                
+        $sqlQuery = 'SELECT * FROM vapoteuses_eliquides';
+        $productsStatement = $mysqlConnection->prepare($sqlQuery);
+        $productsStatement->execute();
+        $products = $productsStatement->fetchAll();
+                
+?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
 
-    bonjour je suis content
-</body>
+        bonjour je suis content
+        <table border="1" width="90%;" >
+            <tbody>
+                <?php 
+        foreach ($products as $product) {
+          ?> <tr> <?php 
+            for ($i=0; $i <= count($products); $i++) { ?>
+                    <td><?php echo $product[$i]; ?></td>
+                <?php } ?>
+                </tr>
+                <?php
+        }
+        ?>
+            </tbody>
+        </table>
+    </body>
 </html>
-<?php 
-try{
-$mysqlConnection = new PDO(
-    'mysql:host=localhost;dbname=VapFactory;charset=utf8',
-    'admin',
-    'adminpwd'
-);
-}
-catch (Exception $e)
-{
-        die('Erreur : ' . $e->getMessage());
-}
-?>
