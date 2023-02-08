215f06da4ea75c3294658e170f50da0649c4cdd8
MehdiYsl <mehdi.yansli@gmail.com>
2023-02-08 14:44:50 +0100

    Ajout du doc css et du dossier images avec le fond

--- index.php
+++ index.php
@@ -25,11 +25,15 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
         <title>Vap Factory</title>
     </head>
     <body>
+        <header>
+            <h1>VAP FACTORY</h1>
+        </header>
 
-        <table border="1" width="90%;" >
+        <table>
             <thead>
                 <tr> 
                     <?php
@@ -41,8 +45,8 @@
                     </th>
                 <?php } ?>
                 </tr>
-                
             </thead>
+
             <tbody>
                 <?php 
         foreach ($products as $product) {
