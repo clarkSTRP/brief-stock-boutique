84aa622ebf4b9a8c81fbc57a49fb941b873ce452
MehdiYsl <mehdi.yansli@gmail.com>
2023-02-09 09:35:16 +0100

    Petits changements dans le script js

--- index.php
+++ index.php
@@ -44,14 +44,25 @@ $stock = $_GET["stock"] ?? null;
         <form action="">
             <div class="line1">
                 <input type="number" name="reference" id="reference" placeholder="Référence">
-                <input type="text" placeholder="Nom du produit" name="nom_article"> 
+                <input type="text" placeholder="Nom du produit" name="nom_article">
             </div>
             <div class="line2">
-                <textarea name="description_article" id="description_article" placeholder="Description du produit"></textarea>
+                <textarea
+                    name="description_article"
+                    id="description_article"
+                    placeholder="Description du produit"></textarea>
             </div>
             <div class="line3">
-                <input type="number" name="prix_achat" id="prix_achat" placeholder="Prix d'achat">
-                <input type="number" name="prix_vente" id="prix_vente" placeholder="Prix de vente">
+                <input
+                    type="number"
+                    name="prix_achat"
+                    id="prix_achat"
+                    placeholder="Prix d'achat">
+                <input
+                    type="number"
+                    name="prix_vente"
+                    id="prix_vente"
+                    placeholder="Prix de vente">
             </div>
             <div class="line4">
                 <input type="number" name="stock" id="stock" placeholder="Stock produit">
@@ -59,43 +70,57 @@ $stock = $_GET["stock"] ?? null;
             </div>
         </form>
 
-        <table>
-            <thead>
-                <tr> 
-                    <?php
-                for ($i=0; $i < $columnHead; $i++) { ?>
-                    <th>
-                       <?php 
-                         echo $productsStatement->getColumnMeta($i)["name"];
-                        ?> 
-                    </th>
-                <?php } ?>
-                </tr>
-            </thead>
+        <form action="" method="get">
 
-            <tbody>
-                <?php 
-        foreach ($products as $product) {
-          ?> <tr> <?php 
-            for ($i=0; $i < count($products); $i++) { ?>
-                    <td><?php echo $product[$i]; ?></td>
-                <?php 
-            } ?>
-                <td><button>Modifier</button></td>
-                <td><button>Supprimer</button></td>
-                </tr>
-        <?php
-        }
-        ?>
-            </tbody>
-        </table>
+            <table>
+                <thead>
+                    <tr>
+                        <?php
+                    for ($i=0; $i < $columnHead; $i++) { ?>
+                        <th>
+                            <?php 
+                             echo $productsStatement->getColumnMeta($i)["name"];
+                            ?>
+                        </th>
+                        <?php } ?>
+                    </tr>
+                </thead>
+    
+                <tbody>
+                    <?php 
+            foreach ($products as $product) {
+              ?>
+                    <tr>
+                        <?php 
+                for ($i=0; $i < count($products); $i++) { ?>
+                        <td><?php echo $product[$i]; ?></td>
+                        <?php 
+                } ?>
+                        <td>
+                            <button id="modif">Modifier</button>
+                            <button id="supp">Supprimer</button>
+                        </td>
+                    </tr>
+                    <?php
+            }
+            ?>
+                </tbody>
+            </table>
+        </form>
 
         <script>
             let addButton = document.getElementsByClassName('ajouter')[0]
 
             addButton.addEventListener('click', createProduct)
-        
-            function createProduct($reference, $nom_article, $description_article, $prix_achat, $prix_vente, $stock) {
+
+            function createProduct(
+                $reference,
+                $nom_article,
+                $description_article,
+                $prix_achat,
+                $prix_vente,
+                $stock
+            ) {
                 <?php try {
        
                 $sql = " INSERT INTO vapoteuses_eliquides (reference, nom_article, description_article, prix_achat, prix_vente, stock) 
@@ -105,7 +130,34 @@ $stock = $_GET["stock"] ?? null;
                 catch(PDOException $e) {
                 echo $sql . "<br>" . $e->getMessage();
                 } ?>
+            }
+
+            let modifButton = document.getElementById('modif')
+            let trTable = document.querySelectorAll('tr')
+            let tdTable = document.querySelectorAll('td')
+
+            let newp = document.createElement('p')
+
+            modifButton.addEventListener('click', updateProduct)
+
+            function updateProduct($id, $reference, $nom_article, $description_article, $prix_achat, $prix_vente, $stock) {
+        <?php
+        try {
+        $sql = "UPDATE vapoteuses_eliquides set 
+                    reference = '$reference',
+                    nom_article = '$nom_article',
+                    description_article = '$description_article',
+                    prix_achat = '$prix_achat',
+                    prix_vente = '$prix_vente',
+                    stock = '$stock',
+                    where id = '$id' ";
+        $stmt = $mysqlConnection->query($sql);
     }
+    catch(PDOException $e) {
+        echo $sql . "<br>" . $e->getMessage();
+    }
+    ?>
+}
         </script>
     </body>
 </html>
