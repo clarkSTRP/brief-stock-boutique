7d830006de5c78821d186ade29d30c1b99ad743b
clarkSTRP <clarkSTRP~>
2023-02-10 11:56:00 +0100

    maj des class css

--- update.php
+++ update.php
@@ -37,10 +37,11 @@ if (isset($_POST['submit'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="updatestyle.css">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
+    <h1 class="titreUpdate" >mise a jour du produit</h1>
     <form class="updateform" action="" method="post">
         <input type="number" name="reference" value="<?php echo $updateReference ?>">
         <input type="text" name="nom_article" value="<?php echo $updateNomArticle ?>">
